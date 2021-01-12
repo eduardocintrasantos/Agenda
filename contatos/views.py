845290a1f6bfb5277fc68ff1f821a51d101a648b
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2021-01-12 12:10:05 -0300

    Initial commit

---
+++ contatos/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import Http404
+from .models import Contato
+
+# Create your views here.
+def index(request):
+    contatos = Contato.objects.all
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404
